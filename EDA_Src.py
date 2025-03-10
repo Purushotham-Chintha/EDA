fc0185ddd5b1ec5de104e0112612eae4fa9018f3
Purushotham Chintha <puruchintha@gmail.com>
2025-03-10 16:12:48 +0530

    updated files

--- EDA_Src.py
+++ EDA_Src.py
@@ -6,23 +6,49 @@ import os
 
 
 home_dir = os.path.expanduser("~")
-file_path = os.path.join(home_dir,"Downloads", "Sample - Superstore.csv")
+file_path = os.path.join(home_dir, "Downloads", "Sample - Superstore.csv")
 
 with open(file_path, "rb") as f:
     rawdata = f.read()
     result = chardet.detect(rawdata)
 
-"""print(result)""" # Detects the best encoding
+"""print(result)"""  # Detects the encoding of our dataset
 
-df = pd.read_csv(file_path, encoding = result["encoding"])
-print(df.sample(40))
+df = pd.read_csv(file_path, encoding=result["encoding"])
 
+# Print the shape of the dataframe (r * c)
+print(df.shape)
+
+# Print the information about the dataframe
 print(df.info())
 
+# Select the top 10 rows of dataframe
+print(df.head(10))
+
+# Select the bottom 10 rows of a dataframe
+print(df.tail(10))
+
+# Select the 10 random rows of a dataframe
 print(df.sample(10))
 
-print(df.dtypes)
+# Drop all the duplicate rows from the dataframe
+df = df.drop_duplicates()  # Returns a dataframe with duplicates removed
 
-print(df.describe())
+"""df.drop_duplicates(inplace=True)"""  # modify the DataFrame rather than creating a new one.
 
-print(df.isnull)
+# print(df.dtypes)
+#
+# print(df.describe())
+#
+# print(df.isnull)
+
+# print(df.sample(40))
+
+"""date parsing from string format to datetime object  allowing for
+specific date operations"""
+
+df["Ship Date"] = pd.to_datetime(df["Ship Date"])
+
+# select and check the type of ship date column
+Ship_Date = df["Ship Date"]
+# print(Ship_Date)
